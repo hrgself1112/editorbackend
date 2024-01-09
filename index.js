ef3a96978c023d3382b953a0a2ec5eeb54c5c130
hrgself1112 <hrgself11.12@gmail.com>
2024-01-09 10:29:05 +0530

    vercel code tested

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express  = require("express")
-const cors = require('cors');
+const app = require('express')();
 
-const port = 7800
-const app = express()
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-app.get("/api/ok" , (req ,res)=>{
-res.send("hii")    
-})
-
-
-app.listen(port, () => {
-    console.log(`Express.js backend is listening on port ${port}`);
-  });
-  
+module.exports = app;
